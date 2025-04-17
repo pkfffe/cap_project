6cd5a44c9cce5c4dd2862701c36f1d7c67c1b84d
PGM <qwe101801@naver.com>
2025-04-17 23:12:16 +0900

    app용으로 교체체

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
