5a8a23092bd3b5dbb2066a79e36082198d76f7fa
PGM <qwe101801@naver.com>
2025-04-17 22:37:24 +0900

    수정정

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <<div className="font-[family-name:var(--font-geist-sans)]">
+    <div className="font-[family-name:var(--font-geist-sans)]">
       <div>1. font-[family-name:var(--font-geist-sans 안녕)]</div>
       <div className="font-[family-name:var(--font-geist-mono)]">
         2. font-[family-name:var(--font-geist-mono 안녕)]
       </div>
-    </div>>
+    </div>
   );
 }
