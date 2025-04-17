d0d941c5fc4bd71630c97b1781f55536f6353fd1
PGM <qwe101801@naver.com>
2025-04-17 22:41:37 +0900

    5

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <div>1. font-[family-name:var(--font-geist-sans 안녕)]</div>
+      <div>2. font-[family-name:var(--font-geist-sans 안녕)]</div>
     </div>
   );
 }
