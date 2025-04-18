2b8b75bf4d41ca660710525074b394ca09d6c87d
PGM <qwe101801@naver.com>
2025-04-18 10:16:18 +0900

    색상 변경경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,25 +4,28 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#c69c6d]">
       <div className="bg-[#e8d7c3] p-8 rounded-xl shadow-lg w-[400px]">
-        <h1 className="text-center text-2xl font-bold mb-6">로그인</h1>
+        <h1 className="text-center text-2xl font-bold mb-6 text-black">
+          로그인
+        </h1>
         <form className="space-y-4">
           <div>
             <label
               htmlFor="username"
-              className="block text-lg font-medium mb-2"
+              className="block text-lg font-medium mb-2 text-black"
             >
               아이디
             </label>
             <input
               type="text"
               id="username"
-              className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring focus:ring-[#c69c6d]"
+              className="w-full border border-gray-50 rounded-lg p-2 focus:outline-none focus:ring focus:ring-[#c69c6d]
+               bg-white"
             />
           </div>
           <div>
             <label
               htmlFor="password"
-              className="block text-lg font-medium mb-2"
+              className="block text-lg font-medium mb-2 text-black"
             >
               비밀번호
             </label>
